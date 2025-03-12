333453fb3b7dece6d4765471a10aa7a60d05dc32
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-12 22:56:44 +0100

    User and Tasks updated

--- join_app/models.py
+++ join_app/models.py
@@ -7,9 +7,7 @@ class User(models.Model):
     name = models.CharField(max_length=50, blank=False)
     e_mail = models.EmailField(blank=False, max_length=254)
     phone = PhoneNumberField(unique=True, blank=False, region="DE")
-    avatar_color = models.CharField(default="#000000")
-
-
+    avatar_color = models.CharField(max_length=10, default="#000000")
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
@@ -25,8 +23,18 @@ class Task(models.Model):
 
     title = models.CharField(max_length=250)
     description = models.TextField()
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ManyToManyField(User, related_name="tasks")
     due_date = models.DateField()
     priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='MEDIUM')
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     subtask = models.TextField()
+    summary_place = models.CharField(max_length=5, default="to-do")
+
+class Summary(models.Model):
+    to_do = models.IntegerField(default=0)
+    done = models.IntegerField(default=0)
+    tasks_in_progress = models.IntegerField(default=0)
+    next_task = models.IntegerField(default=0)
+    await_feedback = models.IntegerField(default=0)
+    tasks_on_board = models.IntegerField(default=0)
+
