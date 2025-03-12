1ee33f31bf57acc0dda248a1b587983d1fbd209b
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-12 14:01:55 +0100

    CATEGORY_CHOICES

--- join_app/models.py
+++ join_app/models.py
@@ -10,13 +10,19 @@ class Task(models.Model):
         ('MEDIUM', 'Medium'),
         ('LOW', 'Low'),
     ]
+    
+    CATEGORY_CHOICES = [
+        ('TECHNICAL_TASK', 'Technical Task'),
+        ('USER_STORY', 'User Story'),        
+    ]
+
 
     title = models.CharField(max_length=250)
     description = models.TextField()
     assigned_to = models.CharField(max_length=100)
     due_date = models.DateField()
-    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='MEDIUM')
-    category = models.CharField(max_length=100)
+    priority = models.CharField(choices=PRIORITY_CHOICES, default='MEDIUM')
+    category = models.CharField(choices=PRIORITY_CHOICES, default='TASK')
     subtask = models.CharField(max_length=100)
 
 class Contact(models.Model):
