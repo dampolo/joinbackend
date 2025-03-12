bb90905f67793f277cbb6b0a7a51e20514e5615b
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-12 11:59:23 +0100

    class Task

--- join_app/models.py
+++ join_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    titel = models.CharField(max_length=30)
+    description = models.CharField(max_length=350)
+    assigned_to = models.CharField()
+    due_date = models.DateField()
+    prio = models.CharField()
+    category = models.CharField()
+    subtask = models.CharField()
