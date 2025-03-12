8a1ece8d131686f304960c70b9048887fb603e71
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-12 15:31:43 +0100

    ForeignKey

--- join_app/models.py
+++ join_app/models.py
@@ -3,6 +3,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 
+class User(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    e_mail = models.EmailField(blank=False, max_length=254)
+    phone = PhoneNumberField(unique=True, blank=False, region="DE")
+    avatar_color = models.CharField(default="#000000")
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
@@ -19,14 +24,8 @@ class Task(models.Model):
 
     title = models.CharField(max_length=250)
     description = models.TextField()
-    assigned_to = models.CharField(max_length=100)
+    assigned_to = models.ForeignKey(User)
     due_date = models.DateField()
     priority = models.CharField(max_length="10", choices=PRIORITY_CHOICES, default='MEDIUM')
     category = models.CharField(max_length="20", choices=CATEGORY_CHOICES)
     subtask = models.TextField()
-
-class Contact(models.Model):
-    name = models.CharField(max_length=50, blank=False)
-    e_mail = models.EmailField(blank=False, max_length=254)
-    phone = PhoneNumberField(unique=True, blank=False, region="DE")
-    avatar_color = models.CharField(default="#000000")
