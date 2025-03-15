5056c26ccf7c14643005e371f3e44db8f14aea4d
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-15 20:47:28 +0100

    PUT and DELETE for users ready

--- join_app/api/urls.py
+++ join_app/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import user_view
+from .views import get_user, get_users, create_user, delete_user, update_user
 
 urlpatterns = [
-    path('', user_view )
+    path('users/', get_users, name="get_users"),   # Get all users
+    path('users/<int:pk>/', get_user, name="get_user"),  
+    path('users/create/', create_user, name="create_user"),
+    path('users/delete/<int:pk>/', delete_user, name="delete_user"),
+    path('users/update/<int:pk>/', update_user, name="update_user")
 ]
--- join_app/api/views.py
+++ join_app/api/views.py
@@ -1,22 +1,66 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializer import UserSerializer
-from join_app.models import User
-
-@api_view(["GET", "POST"])
-def user_view(request):
+from django.shortcuts import get_object_or_404
+from .serializer import UserSerializer, TaskSerializer
+from join_app.models import User, Task
 
 
+# Get a single user:
+@api_view(["GET"])
+def get_user(request, pk):
+    if request.method == "GET":
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+# Get all users:
+@api_view(["GET"])
+def get_users(request):
     if request.method == "GET":
         user = User.objects.all()
         serializer = UserSerializer(user, many=True)
         return Response(serializer.data)
+
+# Create new user
+@api_view(["POST"])
+def create_user(request):
     if request.method == "POST":
         serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+
+# Delete user
+@api_view(["GET", "DELETE"])
+def delete_user(request, pk):
+    user = User.objects.get(pk=pk)
+    serializer = UserSerializer(user)
+    
+    if request.method == "GET":
+        return Response(serializer.data)
+    
+    if request.method == "DELETE":
+        user.delete()
+        return Response(serializer.data)
+
+# Update user
+@api_view(["GET", "PUT"])
+def update_user(request, pk):
+    
+    if request.method == "GET":
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    if request.method == "PUT":
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-            
