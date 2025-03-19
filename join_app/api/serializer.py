52be90bb778739db0b5cc003d146ec333ee5071a
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-19 23:09:00 +0100

    UserSerializer

--- join_app/api/serializer.py
+++ join_app/api/serializer.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from join_app.models import User, Task, Subtask
 
 
-
-
 class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
@@ -12,15 +10,29 @@ class SubtaskSerializer(serializers.ModelSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
     assigned_to = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
-    subtasks = SubtaskSerializer(many=True)
+    subtasks = SubtaskSerializer(many=True, required=False)
 
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ["id", 
+                  "title", 
+                  "description", 
+                  "assigned_to", 
+                  "subtasks",
+                  "due_date",
+                  "priority",
+                  "category",
+                  "board"]
 
 
 class UserSerializer(serializers.ModelSerializer):
-    tasks = TaskSerializer(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ["id", "name", "email", "phone", "avatar_color", "created_at", "updated_at", "tasks"]
+        fields = ["id", 
+                  "name", 
+                  "email", 
+                  "phone",
+                  "avatar_color", 
+                  "created_at", 
+                  "updated_at", 
+                  "tasks"]
