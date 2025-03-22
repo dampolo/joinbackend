2ae4b7360bdbc0baf0a2514043de19c55e0db5f7
Damian Poloczek <damianpoloczek@gmail.com>
2025-03-22 23:45:17 +0100

    create update

--- join_app/api/serializer.py
+++ join_app/api/serializer.py
@@ -6,11 +6,14 @@ class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
         fields = '__all__'
+        extra_kwargs = {"task": {"required": False}}
 
 
 class TaskSerializer(serializers.ModelSerializer):
     assigned_to = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
-    subtasks = SubtaskSerializer(many=True)
+    subtasks = serializers.PrimaryKeyRelatedField(many=True, queryset=Subtask.objects.all())
+
+    # subtasks = SubtaskSerializer(many=True)
 
     class Meta:
         model = Task
@@ -25,17 +28,19 @@ class TaskSerializer(serializers.ModelSerializer):
                   "board"]
         
     def create(self, validated_data):
-        subtasks_data = validated_data.pop('subtasks')
+        subtasks_data = validated_data.pop('subtasks', [])
+        assigned_to_data = validated_data.pop('assigned_to', [])
+
         print(f"Subtasks Data: {subtasks_data}")  # Debugging
+        print(f"Assigned To Data: {assigned_to_data}")  # Debugging
 
         task = Task.objects.create(**validated_data)
-        print(f"Created Task: {task}")  # Debugging
-
+        task.assigned_to.set(assigned_to_data)
 
         for subtask_data in subtasks_data:
             Subtask.objects.create(task=task, **subtask_data)
-        return task
 
+        return task
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
