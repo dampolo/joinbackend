140573057c13ea81f26cd631e35240cf4b745fc4
Damian Poloczek <damianpoloczek@gmail.com>
2025-04-03 23:39:41 +0200

    implement of ViewSet

--- join_app/api/urls.py
+++ join_app/api/urls.py
@@ -1,27 +1,11 @@
-from django.urls import path
-from .views import get_user, get_users, create_user, delete_user, update_user,\
-                TaskListCreateView, TaskDetailView, create_task, update_task,\
-                delete_task, get_summary
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import UsersViewSet, TasksViewSet
+
+router = SimpleRouter()
+router.register(r'users', UsersViewSet, basename="users")  # Users routes
+router.register(r'tasks', TasksViewSet, basename="tasks")  # Tasks routes
 
 urlpatterns = [
-    #USERS:
-    path('users/', get_users, name="get_users"), #zostaje
-    path('users/<int:pk>/', get_user, name="get_user"), # get all data from user
-    path('users/', create_user, name="create_user"), #add new user
-    path('users/<int:pk>/', delete_user, name="delete_user"), #delete user
-    path('users/<int:pk>/', update_user, name="update_user"), #update user
-
-
-    #TASKS:
-    path("tasks/", TaskListCreateView.as_view(), name="task-list-create"),
-    path("tasks/<int:pk>/", TaskListCreateView.as_view(), name="task-list-create"),
-    path("tasks/", create_task, name="create-create"),
-    path("tasks/<int:pk>/", delete_task, name="delete-detail"),
-    path("tasks/<int:pk>/", update_task, name="update-detail"),
-
-    path("summary/", get_summary, name="get-summary"),
-
-    # path("login/", login),
-    # path("registry/", registry)
-    # path("logout/", logout)
+    path("", include(router.urls)),
 ]
--- join_app/api/views.py
+++ join_app/api/views.py
@@ -1,73 +1,100 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
 from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from .serializer import UserSerializer, TaskSerializer, SubtaskSerializer
 from join_app.models import User, Task, Subtask
 
 
-# Get a single user:
-@api_view(["GET"])
-def get_user(request, pk):
-    if request.method == "GET":
-        user = get_object_or_404(User, pk=pk)
+class UsersViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
-
-# Get all users:
-@api_view(["GET"])
-def get_users(request):
-    if request.method == "GET":
-        user = User.objects.all()
-        serializer = UserSerializer(user, many=True)
-        return Response(serializer.data)
-
-
-# Create new user
-@api_view(["POST"])
-def create_user(request):
-    if request.method == "POST":
+    def create(self, request):
         serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else:
-        return Response(serializer.errors)
-
-
-# Delete user
-@api_view(["GET", "DELETE"])
-def delete_user(request, pk):
-    user = User.objects.get(pk=pk)
-    serializer = UserSerializer(user)
-    
-    if request.method == "GET":
-        return Response(serializer.data)
-    
-    if request.method == "DELETE":
-        user.delete()
-        return Response(serializer.data)
-
-
-# Update user
-@api_view(["GET", "PUT"])
-def update_user(request, pk):
-    
-    if request.method == "GET":
-        user = User.objects.get(pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-    if request.method == "PUT":
-        user = User.objects.get(pk=pk)
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+    
+    def destroy(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        user.delete()
+        return Response(serializer.data)
+
+# Get a single user:
+# @api_view(["GET"])
+# def get_user(request, pk):
+#     if request.method == "GET":
+#         user = get_object_or_404(User, pk=pk)
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
+
+
+# Get all users:
+# @api_view(["GET"])
+# def get_users(request):
+#     if request.method == "GET":
+#         user = User.objects.all()
+#         serializer = UserSerializer(user, many=True)
+#         return Response(serializer.data)
+
+
+# Create new user
+# @api_view(["POST"])
+# def create_user(request):
+#     if request.method == "POST":
+#         serializer = UserSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     else:
+#         return Response(serializer.errors)
+
+
+# Delete user
+# @api_view(["GET", "DELETE"])
+# def delete_user(request, pk):
+#     user = User.objects.get(pk=pk)
+#     serializer = UserSerializer(user)
+    
+#     if request.method == "GET":
+#         return Response(serializer.data)
+    
+#     if request.method == "DELETE":
+#         user.delete()
+#         return Response(serializer.data)
+
+
+# Update user
+# @api_view(["GET", "PUT"])
+# def update_user(request, pk):
+    
+#     if request.method == "GET":
+#         user = User.objects.get(pk=pk)
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
+#     if request.method == "PUT":
+#         user = User.objects.get(pk=pk)
+#         serializer = UserSerializer(user, data=request.data, partial=True)
+        
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
 
 
 # Handles both GET (list) and POST (create)
