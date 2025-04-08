e2afe5dbbdb4d654b880cc7833039735412c48cf
Damian Poloczek <damianpoloczek@gmail.com>
2025-04-08 23:03:05 +0200

    change view to ModalViewSet

--- join_app/api/views.py
+++ join_app/api/views.py
@@ -7,75 +7,11 @@ from .serializer import UserSerializer, TaskSerializer, SubtaskSerializer
 from join_app.models import User, Task, Subtask
 
 
-class UsersViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-    
-    def destroy(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        user.delete()
-        return Response(serializer.data)
-    
-    def update(self, request, pk=None):
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class TasksViewSet(viewsets.ViewSet):
+class TasksViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
-    def list(self, request):
-        queryset = Task.objects.all()
-        serializer = TaskSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Task.objects.all()
-        task = get_object_or_404(queryset, pk=pk)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-    
-    def destroy(self, request, pk=None):
-        queryset = Task.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = TaskSerializer(user)
-        user.delete()
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        task = get_object_or_404(Task, pk=pk)
-        serializer = TaskSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
