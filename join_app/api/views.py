bffd8684d0e37b398abfff2b15266ac420c2a304
Damian Poloczek <damianpoloczek@gmail.com>
2025-04-11 23:27:14 +0200

    RegistrationSerializer

--- join_app/api/views.py
+++ join_app/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import viewsets, status
 from django.shortcuts import get_object_or_404
@@ -14,4 +14,4 @@ class UsersViewSet(viewsets.ModelViewSet):
 class TasksViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
--- user_auth_app/api/serializer.py
+++ user_auth_app/api/serializer.py
@@ -5,4 +5,16 @@ from django.contrib.auth.models import User
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['user', 'bio', 'location']
+        fields = '__all__'
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        Model = User
+        fields = ["username", "email", "password", "repeated_password"]
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
